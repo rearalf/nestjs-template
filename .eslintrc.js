52831b9439fdca7482d305b454cfc507dd485984
Ricardo Alf <rear_recinos@hotmail.com>
2024-12-06 21:54:51 -0600

    refactor: Change eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,12 +14,25 @@ module.exports = {
   env: {
     node: true,
     jest: true,
+    browser: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'dist/**', 'node_modules/**'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    'no-console': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: { regex: '^I[A-Z]', match: true },
+      },
+    ],
   },
 };
